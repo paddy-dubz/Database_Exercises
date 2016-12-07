070dba7e9091aa455959e9bac64c5574a9597d97
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-12-07 16:27:06 -0600

    Update where_exercise.sql

--- mysql/where_exercises.sql
+++ mysql/where_exercises.sql
@@ -10,16 +10,14 @@ OR first_name ='Maya';
 SELECT *
 from employees
 WHERE last_name LIKE 'E%'
-OR last_name LIKE '%e';
+AND last_name LIKE '%e';
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%12-25%';
 
 SELECT *
 FROM employees
-WHERE birth_date LIKE '%12-25%';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
