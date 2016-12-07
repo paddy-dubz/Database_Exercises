8e7c0dca7cdfc7221f9c314dd7e22d1534de4980
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-12-07 15:53:49 -0600

    Update where_exercise.sql

--- mysql/where_exercises.sql
+++ mysql/where_exercises.sql
@@ -1,17 +1,21 @@
 USE employees;
 
-SELECT first_name
+SELECT *
 from employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT last_name
+SELECT *
 from employees
 WHERE last_name LIKE 'E%';
 
-SELECT hire_date
+SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
-SELECT birth_date
+SELECT *
 FROM employees
-WHERE birth_date LIKE '%12-25%'
+WHERE birth_date LIKE '%12-25%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
