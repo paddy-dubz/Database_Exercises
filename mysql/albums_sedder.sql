5b1c714f15852838520cf5c0d091e0e3e42ab7a2
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-12-07 10:46:33 -0600

    Updated albums_seeder.sql, also just noticed I can't spell :/

--- mysql/albums_sedder.sql
+++ mysql/albums_sedder.sql
@@ -7,7 +7,30 @@ INSERT INTO albums (artist, name, release_date, sales, genre)
       ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock, Progressive Rock'),
       ('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock, Soft Rock, Folk Rock'),
       ('Bee Gees', 'Saturday Night Fever', 1977, 20.6, 'Disco'),
-      ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock');
+      ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock'),
+      ('Shania Twain',	'Come On Over',	1997, 29.6, 'Country, Pop'),
+      ('Led Zeppelin',	'Led Zeppelin IV',	1971, 29, 'Hard Rock, Heavy Metal'),
+      ('Adele',	'21',	2011, 25.3, 'Pop, Soul'),
+      ('Michael Jackson', 'Bad', 1987, 20.3, 'Pop, Funk, Rock'),
+      ('Alanis Morissette', 'Jagged Little Pill', 1995, 24.8, 'Alternative Rock'),
+      ('Celine Dion', 'Falling Into You', 1996, 20.2, 'Pop, Soft Rock'),
+      ('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', 1967, 13.1, 'Rock'),
+      ('Eagles', 'Hotel California', 1976, 21.5, 'Rock, Soft Rock, Folk Rock'),
+      ('Mariah Carey', 'Music Box', 1993, 19, 'Pop, R&B, Rock'),
+      ('Michael Jackson', 'Dangerous', 1991, 17.6, 'Rock, Funk, Pop'),
+      ('Various Artists', 'Dirty Dancing', 1987, 17.9, 'Pop, Rock, R&B'),
+      ('Celine Dion', 'Lets Talk About Love', 1997, 19.3, 'Pop, Soft Rock'),
+      ('The Beatles', '1', 2000, 21.6, 'Rock'),
+      ('The Beatles', 'Abbey Road', 1969, 14.4, 'Rock'),
+      ('Bruce Springsteen', 'Born In the USA', 1984, 19.6, 'Rock'),
+      ('Dire Straits', 'Brothers in Arms', 1985, 17.7, 'Rock'),
+      ('James Horner', 'Titanic: Music from the Motion Picture', 1997, 18.1, 'Soundtrack'),
+      ('Madonna', 'The Immaculate Collection', 1990, 19.4, 'Pop, Dance'),
+      ('Metallica', 'Metallica', 1991, 19.9, 'Thrash Metal, Heavy Metal'),
+      ('Nirvana', 'Nevermind', 1991, 16.7,'Grunge, Alternative Rock'),
+      ('Pink Floyd', 'The Wall', 1979, 17.6, 'Progressive Rock'),
+      ('Santana', 'Supernatural', 1999, 20.5, 'Rock'),
+      ('Guns N Roses', 'Appetite for Destruction', 1987, 21.3, 'Heavy Metal, Hard Rock')
 # SHOW TABLES;
 # DESCRIBE albums;
 # SHOW INDEX FROM albums;
