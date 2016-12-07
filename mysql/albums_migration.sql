27426a63765fae1d43cc4f05361390b73056a59d
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-12-07 10:09:33 -0600

    Added albums_seeder.sql and updated albums_migration.sql

---
+++ mysql/albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+ id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+ artist VARCHAR(50) NOT NULL,
+ name VARCHAR(50)  NOT NULL,
+ release_date YEAR(4) NOT NULL,
+ sales DECIMAL(8,2) NOT NULL,
+ genre VARCHAR(50) NOT NULL,
+ PRIMARY KEY (id)
+);
+# USE codeup_test_db;
+SHOW TABLES;
+DESCRIBE albums;
---
+++ mysql/albums_sedder.sql
@@ -0,0 +1,14 @@
+use codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+    VALUES ('Michael Jackson', 'Thriller', 1982, 46, 'Pop, Rock, R&B'),
+      ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard Rock'),
+      ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive Rock'),
+      ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B, Soul, Pop, Soundtrack'),
+      ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock, Progressive Rock'),
+      ('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock, Soft Rock, Folk Rock'),
+      ('Bee Gees', 'Saturday Night Fever', 1977, 20.6, 'Disco'),
+      ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock');
+# SHOW TABLES;
+# DESCRIBE albums;
+# SHOW INDEX FROM albums;
+# SELECT * FROM albums;
