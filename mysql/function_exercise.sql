65cd91667dc2b1512410cdbd3967f0854791220d
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-12-08 14:02:36 -0600

    Added function_exercise.sql

---
+++ mysql/function_exercise.sql
@@ -0,0 +1,31 @@
+# noinspection SqlResolveForFile
+USE employees;
+
+SELECT COUNT(gender)
+from employees
+WHERE first_name = 'Irena'
+  OR first_name = 'Vidya'
+  OR first_name = 'Maya'
+group by gender;
+
+SELECT *
+from employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC ;
+
+SELECT concat(first_name, ' ', last_name)
+from employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%e';
+
+SELECT datediff(hire_date, curdate())
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%12-25%'
+ORDER BY birth_date ASC, hire_date DESC ;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%'
+GROUP BY first_name, last_name;
